0cc1533030e6252960fc03b208e9982cb49337cc
KanDai <KanDai@users.noreply.github.com>
2023-09-28 14:02:00 +0900

    animateのclassを追加

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,32 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      animation: {
+        "scroll-down-line":
+          "scroll-down-line 1.5s infinite",
+      },
+      keyframes: {
+        "scroll-down-line": {
+          "0%": {
+            transform: "scale(1,0)",
+            transformOrigin: "0 0",
+          },
+          "50%": {
+            transform: "scale(1,1)",
+            transformOrigin: "0 0",
+          },
+          "50.1%": {
+            transform: "scale(1,1)",
+            transformOrigin: "0 100%",
+          },
+          "100%": {
+            transform: "scale(1,0)",
+            transformOrigin: "0 100%",
+          },
+        },
+      },
+    },
   },
   plugins: [],
 }
